af5c6fcdb9296ea64465f52de9a99452294ad8cb
Taylor Pasquariello <taylor.pasquariello@spiredigital.com>
2020-11-13 13:32:44 -0700

    set up block plugin gitignore, build and sample

--- wp-sandbox/wp-content/plugins/corteva-blocks/src/sample-block/block.js
+++ wp-sandbox/wp-content/plugins/corteva-blocks/src/sample-block/block.js
@@ -1,17 +1,14 @@
 /**
- * BLOCK: corteva-blocks
+ * BLOCK: my-block
  *
  * Registering a basic block with Gutenberg.
  * Simple block, renders and saves the same content without any interactivity.
  */
-
 //  Import CSS.
 import './editor.scss';
 import './style.scss';
-
 const { __ } = wp.i18n; // Import __() from wp.i18n
 const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
-
 /**
  * Register: aa Gutenberg Block.
  *
@@ -25,17 +22,26 @@ const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.b
  * @return {?WPBlock}          The block, if it has been successfully
  *                             registered; otherwise `undefined`.
  */
-registerBlockType( 'cgb/block-corteva-blocks', {
+const {
+	RichText,
+} = wp.editor;
+registerBlockType( 'cgb/block-my-block', {
 	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-	title: __( 'corteva-blocks - CGB Block' ), // Block title.
-	icon: 'shield', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
+	title: __( 'PE Sample Block' ), // Block title.
+	icon: 'buddicons-replies', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
 	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
 	keywords: [
-		__( 'corteva-blocks — CGB Block' ),
-		__( 'CGB Example' ),
+		__( 'corteva-blocks' ),
+		__( 'Sample Block' ),
 		__( 'create-guten-block' ),
 	],
-
+	// attributes and setAttributes is the gutenberg version of state and setState
+	attributes: {
+		inputValue: {
+			type: 'string',
+			default: ''
+		},
+	},
 	/**
 	 * The edit function describes the structure of your block in the context of the editor.
 	 * This represents what the editor will render when the block is used.
@@ -48,25 +54,24 @@ registerBlockType( 'cgb/block-corteva-blocks', {
 	 * @returns {Mixed} JSX Component.
 	 */
 	edit: ( props ) => {
-		// Creates a <p class='wp-block-cgb-block-corteva-blocks'></p>.
+
 		return (
 			<div className={ props.className }>
-				<p>— Hello from the backend.</p>
-				<p>
-					CGB BLOCK: <code>corteva-blocks</code> is a new Gutenberg block
-				</p>
-				<p>
-					It was created via{ ' ' }
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
+				<h3>— Hello from the backend.</h3>
+				<p>Add text to the input below and preview the page to get a sense for how information is posted</p>
+				<RichText
+					autoFocus="false"
+					className="btn btn--primary"
+					value={props.attributes.inputValue}
+					placeholder="Place Sample Text"
+					onChange={
+						// notice, rather than taking in an event as the first argument it takes in 'value' 
+						(value) => props.setAttributes({ inputValue: value })
+					}
+				/>	
 			</div>
 		);
 	},
-
 	/**
 	 * The save function defines the way in which the different attributes should be combined
 	 * into the final markup, which is then serialized by Gutenberg into post_content.
@@ -82,17 +87,7 @@ registerBlockType( 'cgb/block-corteva-blocks', {
 		return (
 			<div className={ props.className }>
 				<p>— Hello from the frontend.</p>
-				<p>
-					CGB BLOCK: <code>corteva-blocks</code> is a new Gutenberg block.
-				</p>
-				<p>
-					It was created via{ ' ' }
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
+				<p>{props.attributes.inputValue}</p>
 			</div>
 		);
 	},
