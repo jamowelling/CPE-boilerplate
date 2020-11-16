eb0bc1b19cef6c47acf7ed585bdd13b992ab5e50
Taylor Pasquariello <taylor.pasquariello@spiredigital.com>
2020-11-16 03:17:38 -0700

    add angular elements hellpw world example

--- corteva-client-sandbox/src/app/app.module.ts
+++ corteva-client-sandbox/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { createCustomElement } from '@angular/elements';
+import {APP_BASE_HREF} from '@angular/common';  
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +12,7 @@ import { MessageService } from './message.service';
 import { HomeComponent } from './home/home.component';
 import { PagesListComponent } from './pages-list/pages-list.component';
 import { PostsListComponent } from './posts-list/posts-list.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
   declarations: [
@@ -17,17 +20,33 @@ import { PostsListComponent } from './posts-list/posts-list.component';
     TestComponent,
     HomeComponent,
     PagesListComponent,
-    PostsListComponent
+    PostsListComponent,
+    HelloWorldComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule
   ],
+  entryComponents:[HelloWorldComponent],
   providers: [
     HttpErrorHandler,
     MessageService,
+    {provide: APP_BASE_HREF, useValue : '/' }
   ],
-  bootstrap: [AppComponent]
+  //bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(injector: Injector) {
+    const custom = createCustomElement(HelloWorldComponent, {injector: injector});
+    customElements.define('app-hello-world', custom);
+  }
+
+  ngDoBootstrap(appRef): void {
+    if (document.querySelector('app-root')) {
+      appRef.bootstrap(AppComponent);
+    }
+  }
+  
+
+}
