0ec49f65b05a89f6231b2dd4d4d138067d27e657
Jameson <jamowelling@gmail.com>
2020-11-30 12:43:09 -0700

    install scss preprocessors for testing

--- cpe-angular/karma.conf.js
+++ cpe-angular/karma.conf.js
@@ -12,6 +12,7 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-scss-preprocessor'),
       require('karma-viewport'),
     ],
     client: {
